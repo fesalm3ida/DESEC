25615f22d7dc4bcb63aa9e1459bf0092fc4f69a9
Felipe S. Almeida <y3a972u91@relay.firefox.com>
2025-08-02 19:31:00 -0300

    Ajuste de encode e socket ipv4

--- InformationGathering-INFRA/whois.py
+++ InformationGathering-INFRA/whois.py
@@ -1,8 +1,9 @@
 #!/usr/share/python
 import socket
 
-s=socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-s.connect(("2001:12ff:0:2::3",43))
-s.send("businesscorp.com.br"+"\r\n")
-resposta = s.recv(1024)
-print resposta
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("200.160.2.80",43))
+s.send(("businesscorp.com.br"+"\r\n").encode())
+resposta = s.recv(4096).decode('latin-1')
+print (resposta)
+s.close()
